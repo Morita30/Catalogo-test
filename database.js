76297f7301abf3e423eb83583345387df6f99ff5
Morita30 <miltondulantoostos@gmail.com>
2026-04-08 23:28:11 -0500

    corrección 1.js

--- database.js
+++ database.js
@@ -2,15 +2,14 @@
 const imgUrlBase = "https://miniture.b-cdn.net/wp-content/uploads/2023/10/kids_toys_02_1.jpeg";
 
 const productosData = [
-    { c: "pastilla", m: "toyota", n: "Pastilla Hilux Revo", p: 120, e: "m" }, // Más vendido
-    { c: "disco", m: "nissan", n: "Disco Frontier Del", p: 180, e: "o" },   // Oferta
+    { c: "pastilla", m: "toyota", n: "Pastilla Hilux Revo", p: 120, e: "m" },
+    { c: "disco", m: "nissan", n: "Disco Frontier Del", p: 180, e: "o" },
     { c: "amortiguador", m: "toyota", n: "Amortiguador Hilux", p: 250 },
     { c: "radiador", m: "hyundai", n: "Radiador Accent", p: 420, e: "o" },
     { c: "zapata", m: "toyota", n: "Zapata Hilux", p: 130 },
     { c: "embrague", m: "nissan", n: "Kit Embrague NP300", p: 850, e: "m" }
 ];
 
-// Generador de relleno
 while(productosData.length < 54) {
     let base = productosData[Math.floor(Math.random() * productosData.length)];
     productosData.push({...base, n: base.n + " " + productosData.length});
